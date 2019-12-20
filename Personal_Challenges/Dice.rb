2ae25be56532e4877077a46650a4cd4ff683daa3
Fennic08 <59082986+Fennic08@users.noreply.github.com>
2019-12-20 19:10:34 +1000

    Added Dice

---
+++ Personal_Challenges/Dice.rb
@@ -0,0 +1,12 @@
+
+dice6 = ["1", "2", "3", "4", "5", "6"]
+dice8 = ["1", "2", "3", "4", "5", "6", "7", "8"]
+
+
+puts "Black"
+puts dice6.sample(1)
+puts dice8.sample(1)
+
+puts "Red"
+puts dice6.sample(1)
+puts dice8.sample(1)
